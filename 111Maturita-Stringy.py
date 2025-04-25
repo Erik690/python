6d2e876ca86d24129529bc4331afc0c20cc73029
Erik690 <ErikKern@s.zochova.sk>
2025-04-25 10:28:29 +0200

    Python1

---
+++ 111Maturita-Stringy.py
@@ -0,0 +1,14 @@
+#moje_meno = "Erik Kern"
+#pocet_znakov = len(moje_meno)
+#print("Pocet znakov v mojom mene je: ", pocet_znakov)
+#print("Prvy znak je: ", moje_meno[0])
+#print("Posledny znak je: ", moje_meno[-1])
+#print("Prve tri znaky su: ", moje_meno[0:3])
+#print("Prvy znak je: ", moje_meno[:1])
+#print("Posledny znak je: ", moje_meno[-1:])
+
+Meno = "Erik"
+Priezvisko = "Kern"
+Cele_meno = Meno + " " + Priezvisko
+print(Cele_meno)
+print(len(Cele_meno))
